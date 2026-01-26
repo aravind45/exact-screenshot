fdd3807d22627da02672d2e81131022b8a08fc3e
aravind45 <37077333+aravind45@users.noreply.github.com>
2026-01-26 09:04:57 -0600

    Netlify

--- server/db.ts
+++ server/db.ts
@@ -1,3 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+const prismaClientSingleton = () => {
+    return new PrismaClient({
+        log: ['error', 'warn'],
+    });
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClientSingleton | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
